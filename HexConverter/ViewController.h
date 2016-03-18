c25a5a2166f2efd573b8fcd2421a66d014c3c308
Barret J. Nobel <bear.nobel@gmail.com>
2016-03-18 12:58:08 -0400

    Initial Commit

---
+++ HexConverter/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HexConverter
+//
+//  Created by OSX on 3/18/16.
+//  Copyright © 2016 LebonTech Solutions. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ HexConverter/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  HexConverter
+//
+//  Created by OSX on 3/18/16.
+//  Copyright © 2016 LebonTech Solutions. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
