256ed5f362e2467cedd09dbeeba18bceb2f54963
Barret J. Nobel <bear.nobel@gmail.com>
2016-03-18 13:41:48 -0400

    added some comments tested the button click removed label text

--- ConvertViewController.m
+++ ConvertViewController.m
@@ -10,10 +10,11 @@
 
 @interface ConvertViewController ()
 
-@property (weak, nonatomic) IBOutlet UITextField *hexConvertTextField;
-@property (weak, nonatomic) IBOutlet UILabel *redLabel;
-@property (weak, nonatomic) IBOutlet UILabel *greenLabel;
-@property (weak, nonatomic) IBOutlet UILabel *blueLabel;
+@property (weak, nonatomic) IBOutlet UITextField *hexConvertTextField; //. textField for user input
+@property (weak, nonatomic) IBOutlet UILabel *redLabel; //................ red label to update once converted
+@property (weak, nonatomic) IBOutlet UILabel *greenLabel; //.............. green label to update once converted
+@property (weak, nonatomic) IBOutlet UILabel *blueLabel; //............... blue label to update once converted
+
 @end
 
 @implementation ConvertViewController
@@ -28,6 +29,24 @@
     // Dispose of any resources that can be recreated.
 }
 
+#pragma mark - ConvertButton
+
+// Method to handle the convert button being pressed
+// converts the hex-code input to an array of three normalized floating point numbers
 - (IBAction)convertButton:(id)sender {
+    NSString *hexInput = self.hexConvertTextField.text;
+    
+    NSLog(@"%@", hexInput);
 }
+
+#pragma mark - Helper
+-(void)hexConvert:(NSString *)hexToConvert{
+//    NSArray *normalizedArray = @[ @0.0, @0.0, @0.0 ];
+    
+}
+
+//-(int)normalizeThisNumber:(int)numberToConvert withBase:(int)base{
+//    
+//}
+
 @end
