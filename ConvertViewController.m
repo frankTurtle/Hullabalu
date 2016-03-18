60b13d8fd99f14d077c9afdde6e558a30aa87d97
Barret J. Nobel <bear.nobel@gmail.com>
2016-03-18 15:27:48 -0400

    comments

--- ConvertViewController.m
+++ ConvertViewController.m
@@ -41,41 +41,44 @@ NSMutableArray *normalizedArray; //. variable to hold the array of noramlized fl
 // Method to handle the convert button being pressed
 // converts the hex-code input to an array of three normalized floating point numbers
 - (IBAction)convertButton:(id)sender {
-    NSString *hexInput = self.hexConvertTextField.text;
-    NSArray *convertedFloats;
+    NSString *hexInput = self.hexConvertTextField.text; //............................... gets the input from the user text field
+    NSArray *convertedFloats; //......................................................... variable to hold the array object once returned
     
-    if( [hexInput length] == 6 && [ self isValidHex:[hexInput uppercaseString] ] ){
-        convertedFloats = [self hexConvert:hexInput atIndex:START_INDEX];
-        NSLog(@"%@", convertedFloats);
+    if( [hexInput length] == 6 && [ self isValidHex:[hexInput uppercaseString] ] ){ //... if the input length and values are correct
+        convertedFloats = [self hexConvert:hexInput atIndex:START_INDEX]; //............. convert them to normalized floats and put into array
+        NSLog(@"%@", convertedFloats); //................................................ print array to console
     }
-    else
+    else{//.............................................................................. length or values are not correct
         NSLog(@"False");
+    }
+
 }
 
 #pragma mark - Helper
 
 // Method to convert the value from the TextField to a normalized floating point number
 // populates the array with values as well
+// returns an array of all converted values
 -(NSArray *)hexConvert:(NSString *)hexToConvert atIndex:(int)index{
-    if( [hexToConvert length] == 0 ){
-        self.redLabel.text =   [NSString stringWithFormat:@"%.2f", [normalizedArray[RED_INDEX]   floatValue] ];
+    if( [hexToConvert length] == 0 ){ //................................................................................ this stops the recursive calls
+        self.redLabel.text =   [NSString stringWithFormat:@"%.2f", [normalizedArray[RED_INDEX]   floatValue] ]; //...... sets all label text
         self.greenLabel.text = [NSString stringWithFormat:@"%.2f", [normalizedArray[GREEN_INDEX] floatValue] ];
         self.blueLabel.text =  [NSString stringWithFormat:@"%.2f", [normalizedArray[BLUE_INDEX]  floatValue] ];
     }
     else{
-        NSString *firstTwoCharacters = [hexToConvert substringToIndex:2];
+        NSString *firstTwoCharacters = [hexToConvert substringToIndex:2]; //............................................ get the first two chars of the string to convert
         NSScanner *converter = [NSScanner scannerWithString:firstTwoCharacters];
         
-        unsigned convertedHex = 0;
-        [converter scanHexInt:&convertedHex];
+        unsigned convertedHex = 0; //................................................................................... variable to hold the converted hex value
+        [converter scanHexInt:&convertedHex]; //........................................................................ converts the hex to an int
         
-        NSNumber *convertedNum = [NSNumber numberWithFloat:[self normalizeThisNumber:convertedHex withBase:255.0]];
-        [normalizedArray insertObject:convertedNum atIndex:index];
+        NSNumber *convertedNum = [NSNumber numberWithFloat:[self normalizeThisNumber:convertedHex withBase:255.0]]; //.. variable to convert the int above to a normalized float
+        [normalizedArray insertObject:convertedNum atIndex:index]; //................................................... add normalized value to the array
         
-        [self hexConvert:[hexToConvert substringFromIndex:2] atIndex:++index];
+        [self hexConvert:[hexToConvert substringFromIndex:2] atIndex:++index]; //....................................... recursively call itself with a substring
     }
     
-    return [normalizedArray mutableCopy];
+    return [normalizedArray mutableCopy]; //............................................................................ return the array
 }
 
 // Method to normalize a number passed in with a certain base
